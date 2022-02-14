90415750eeaffc18334d2396e9f4277c1021ee03
eh. sh <sh.ehsan74.es@gmail.com>
2022-02-14 14:07:13 +0330

    add show/ hide password

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <vue-base-input autofocuse type="text" placeholder="name" label="your name" />
-  <vue-base-input type="password" placeholder="password" label="your password" />
-  <vue-base-input type="textarea" placeholder="message" label="your message" />
+  <vue-base-input buttonText="Send" autofocus :type="'text'" placeholder="name" label="your name" />
+  <vue-base-input autofocus :type="'number'" placeholder="number" label="your number" />
+  <vue-base-input v-model="password" maxlength="4" :type="'password'" placeholder="password" label="your password" />
+  <vue-base-input :type="'textarea'" placeholder="message" label="your message" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import VueBaseInput from './components/VueBaseInput.vue';
 
 export default defineComponent({
   name: 'App',
   components: { VueBaseInput },
+  setup() {
+    const password = ref('');
+
+    return { password };
+  },
 });
 </script>
 <style lang="scss">
 * {
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
 </style>
--- src/components/VueBaseInput.vue
+++ src/components/VueBaseInput.vue
@@ -2,31 +2,49 @@
   <div class="vb-input" :style="blockStyle" :class="{ rtl: rtl, error: error }">
     <label :style="labelStyle" class="vb-input__label" v-if="label" :class="{ inline: labelInline }">{{ label }}</label>
     <textarea
-      v-if="$attrs.type === 'textarea'"
+      v-if="type === 'textarea'"
       class="vb-input__field"
+      :type="setFieldType"
       @input="$emit('update:modelValue', $event.target.value)"
       v-bind="$attrs"
       :value="modelValue"
       :style="fieldStyle"
     />
-    <input
-      v-else
-      class="vb-input__field"
-      v-bind="$attrs"
-      :value="modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-      :style="fieldStyle"
-    />
+    <div v-else class="vb-input__block">
+      <input
+        class="vb-input__field"
+        :type="setFieldType"
+        @input="$emit('update:modelValue', $event.target.value)"
+        v-bind="$attrs"
+        :value="modelValue"
+        :style="fieldStyle"
+      />
+      <button
+        v-if="type === 'password' || buttonText"
+        class="vb-input__btn"
+        type="button"
+        :class="{ rtl: rtl }"
+        :style="buttonStyle"
+        @click="onButtonClick"
+      >
+        <span class="vb-input__passwordIcon" v-if="type === 'password'" :class="{ showPassword: showPassword }" />
+        <span v-else>{{ buttonText }}</span>
+      </button>
+    </div>
     <p v-if="error" class="vb-input__errorMessage" :style="errorMessageStyle">{{ error }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'VueBaseInput',
   props: {
+    type: {
+      type: String,
+      required: true,
+    },
     label: {
       type: String,
       default: '',
@@ -71,6 +89,40 @@ export default defineComponent({
         return {};
       },
     },
+    buttonStyle: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+    buttonText: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props) {
+    const showPassword = ref(false);
+
+    const setFieldType = computed(() => {
+      if (props.type === 'password') {
+        if (showPassword.value) return 'text';
+        if (!showPassword.value) return 'password';
+      }
+      return props.type;
+    });
+
+    const onButtonClick = () => {
+      if (props.type === 'password') {
+        showPassword.value = !showPassword.value;
+        return;
+      }
+    };
+
+    return {
+      onButtonClick,
+      showPassword,
+      setFieldType,
+    };
   },
 });
 </script>
@@ -83,14 +135,16 @@ export default defineComponent({
   --vb-input-placeholder-color: #757575;
   --vb-input-font-size: 16px;
   --vb-input-field-border-radius: 4px;
+  --vb-input-height: 40px;
   --vb-input-textarea-height: 80px;
+  --vb-input-button-width: 64px;
 }
 .vb-input {
   color: var(--vb-input-color-default);
 
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   width: 300px;
-  padding: 100px;
+  margin: 100px;
 
   &.error {
     color: var(--vb-input-color-error);
@@ -117,13 +171,16 @@ export default defineComponent({
     -moz-appearance: none;
     appearance: none;
 
-    height: 36px;
+    height: var(--vb-input-height);
     width: 100%;
-    display: flex;
-    align-items: center;
     margin: 8px 0;
     border: 1px solid var(--vb-input-color-secondary);
     border-radius: var(--vb-input-field-border-radius);
+
+    &:not([type='textarea']) {
+      display: flex;
+      align-items: center;
+    }
 
     &::placeholder {
       color: var(--vb-input-placeholder-color);
@@ -150,6 +207,46 @@ export default defineComponent({
   &__label {
     padding: 0 8px;
     font-size: var(--vb-input-font-size);
+  }
+
+  &__block {
+    position: relative;
+  }
+
+  &__btn {
+    border: none;
+    background-color: transparent;
+    box-shadow: none;
+    outline: none;
+
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 1px;
+    bottom: 1px;
+    right: 1px;
+    width: var(--vb-input-button-width);
+    cursor: pointer;
+
+    &.rtl {
+      left: 1px;
+      right: unset;
+    }
+  }
+
+  &__passwordIcon {
+    display: block;
+    width: 24px;
+    height: 24px;
+    background-size: 24px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='24px' width='24px' viewBox='0 0 24 24' fill='%239e9e9e'%3E%3Cpath d='M0 0h24v24H0V0zm0 0h24v24H0V0zm0 0h24v24H0V0zm0 0h24v24H0V0z' fill='none'/%3E%3Cpath d='M12 6c3.79 0 7.17 2.13 8.82 5.5-.59 1.22-1.42 2.27-2.41 3.12l1.41 1.41c1.39-1.23 2.49-2.77 3.18-4.53C21.27 7.11 17 4 12 4c-1.27 0-2.49.2-3.64.57l1.65 1.65C10.66 6.09 11.32 6 12 6zm-1.07 1.14L13 9.21c.57.25 1.03.71 1.28 1.28l2.07 2.07c.08-.34.14-.7.14-1.07C16.5 9.01 14.48 7 12 7c-.37 0-.72.05-1.07.14zM2.01 3.87l2.68 2.68C3.06 7.83 1.77 9.53 1 11.5 2.73 15.89 7 19 12 19c1.52 0 2.98-.29 4.32-.82l3.42 3.42 1.41-1.41L3.42 2.45 2.01 3.87zm7.5 7.5l2.61 2.61c-.04.01-.08.02-.12.02-1.38 0-2.5-1.12-2.5-2.5 0-.05.01-.08.01-.13zm-3.4-3.4l1.75 1.75c-.23.55-.36 1.15-.36 1.78 0 2.48 2.02 4.5 4.5 4.5.63 0 1.23-.13 1.77-.36l.98.98c-.88.24-1.8.38-2.75.38-3.79 0-7.17-2.13-8.82-5.5.7-1.43 1.72-2.61 2.93-3.53z'/%3E%3C/svg%3E");
+
+    &.showPassword {
+      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 0 24 24' width='24px' fill='%239e9e9e'%3E%3Cpath d='M0 0h24v24H0V0z' fill='none'/%3E%3Cpath d='M12 6c3.79 0 7.17 2.13 8.82 5.5C19.17 14.87 15.79 17 12 17s-7.17-2.13-8.82-5.5C4.83 8.13 8.21 6 12 6m0-2C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 5c1.38 0 2.5 1.12 2.5 2.5S13.38 14 12 14s-2.5-1.12-2.5-2.5S10.62 9 12 9m0-2c-2.48 0-4.5 2.02-4.5 4.5S9.52 16 12 16s4.5-2.02 4.5-4.5S14.48 7 12 7z'/%3E%3C/svg%3E");
+    }
   }
 }
 </style>
